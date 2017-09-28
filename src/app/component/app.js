1d0e0e8bb9ddd38e2c70cd62f021e6c25a430ff0
myoungsubsim <rop2001@gmail.com>
2017-09-28 22:49:54 +0900

    computed & methods 추가

--- src/app/component/app.js
+++ src/app/component/app.js
@@ -1,10 +1,11 @@
 "use strict"
 
 Vue.component('todo-item',{
+  //props는
   //사용자 정의 속성
   props : ['todo'],
   template : '<li>{{todo.text}}</li>'
-})
+});
 
 var component1 = new Vue({
   el : "#app",
@@ -15,10 +16,32 @@ var component1 = new Vue({
       {id : 2, text : 'c++'}
     ]
   }
-})
+});
+
+var component2 = new Vue({
+    el : "#app2",
+    data : {
+        message : "안녕하세요."
+    },
+    //computed속성은
+    //캐시가 되기 때문에 message값이 변하지 않으면
+    //이전에 나온 결과를 그대로 반환한다.
+    computed : {
+        reversedMessage : function(){
+            return this.message.split('').reverse().join('');
+        }
+    },
+    //methods 속성은
+    //캐시상관없이 항상 사용할 때마다 재 호출된다.
+    methods : {
+        reversedMessageMethod : function(){
+            return this.message.split('').reverse().join('');
+        }
+    }
+});
 
 function initialize(){
-  return component1;
+  return component2;
 }
 
 export default initialize;
--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,6 @@
 "use strict"
 
+//base
 import vue_init from './base/app.js';
+//component
 import vue_component_init from './component/app.js';
